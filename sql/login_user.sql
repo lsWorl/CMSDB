8d97f6b59246c33d17cbecef971f3a351b62886c
sink <1428903832@qq.com>
2022-10-07 00:02:35 +0800

    添加数据库

---
+++ sql/login_user.sql
@@ -0,0 +1,3 @@
+INSERT INTO `login_user` VALUES (1, '张三', '13968926965', 1, '123456');
+INSERT INTO `login_user` VALUES (2, 'admin', '13968926444', 0, '123456');
+INSERT INTO `login_user` VALUES (4, '小伙子', '17856347855', 1, '123456');
---
+++ sql/user_contacts.sql
@@ -0,0 +1,5 @@
+INSERT INTO `user_contacts` VALUES (36, 1, '张21', 'https://pic2.zhimg.com/v2-d2aba0f0e32c3462d74bd1c801a81e79_r.jpg?source=1940ef5c', '', 0, '10');
+INSERT INTO `user_contacts` VALUES (35, 1, '张21', 'https://pic2.zhimg.com/v2-d2aba0f0e32c3462d74bd1c801a81e79_r.jpg?source=1940ef5c', '', 0, '10');
+INSERT INTO `user_contacts` VALUES (35, 3, '小李', 'https://n.sinaimg.cn/sinakd10115/83/w743h940/20200809/1bd5-ixkvvue5479978.jpg', '', 0, '1');
+INSERT INTO `user_contacts` VALUES (36, 35, 'user', 'https://pic2.zhimg.com/v2-d2aba0f0e32c3462d74bd1c801a81e79_r.jpg?source=1940ef5c', '', 0, '100');
+INSERT INTO `user_contacts` VALUES (35, 36, 'user', 'https://pic2.zhimg.com/v2-d2aba0f0e32c3462d74bd1c801a81e79_r.jpg?source=1940ef5c', '', 0, '100');
